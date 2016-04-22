29e6b43169056b99731e4de7d2225770ec6965d5
Sphiecoh <sfiso.dlaba@gmail.com>
2016-04-22 12:53:38 +0200

    publish artifacts

--- build.ps1
+++ build.ps1
@@ -2,11 +2,7 @@ param (
 	[Parameter(Mandatory=$true)]
 	[ValidatePattern("^\d+\.\d+\.(?:\d+\.\d+$|\d+$)")]
 	[string]
-	$ReleaseVersionNumber,
-	[Parameter(Mandatory=$true)]
-	[string]
-	[AllowEmptyString()]
-	$PreReleaseName
+	$ReleaseVersionNumber
 )
 
 $PSScriptFilePath = (Get-Item $MyInvocation.MyCommand.Path).FullName
@@ -39,7 +35,7 @@ $DNVM = "dnvm"
 
 # Make sure we don't have a release folder for this version already
 $BuildFolder = Join-Path -Path $SolutionRoot -ChildPath "build";
-$ReleaseFolder = Join-Path -Path $BuildFolder -ChildPath "Releases\v$ReleaseVersionNumber$PreReleaseName";
+$ReleaseFolder = Join-Path -Path $BuildFolder -ChildPath "Releases\v$ReleaseVersionNumber";
 if ((Get-Item $ReleaseFolder -ErrorAction SilentlyContinue) -ne $null)
 {
 	Write-Warning "$ReleaseFolder already exists on your local machine. It will now be deleted."
